8b8643a53a46b8dcf42a47888b83d52271422188
Michael Moncera <michaelmoncera@gmail.com>
2022-06-02 21:04:05 +0000

    Completed react-validated-input-component & bonus challenge

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ValidatedInput from './validated-input.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ValidatedInput />);

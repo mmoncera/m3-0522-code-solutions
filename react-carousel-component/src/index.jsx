d5eb064e91cedde88bcf19a924c41571159b5add
Michael Moncera <michaelmoncera@gmail.com>
2022-06-07 04:03:26 +0000

    Completed react-carousel-component

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Carousel from './Carousel';
+
+const images = [
+  { name: 'bulbasaur', url: '../images/001.png' },
+  { name: 'charmander', url: '../images/004.png' },
+  { name: 'squirtle', url: '../images/007.png' },
+  { name: 'pikachu', url: '../images/025.png' },
+  { name: 'jigglypuff', url: '../images/039.png' }
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Setup Project!!!</h1>);
+root.render(<Carousel list={images}/>);
